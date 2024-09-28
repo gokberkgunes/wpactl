87efcd23780900799d69f3743e2a68192ffbfd54
Gokberk Gunes <prhi2@tutanota.com>
2024-09-28 03:25:54 +0300

    commentation fixes in wpactl.py

--- wpactl.py
+++ wpactl.py
@@ -4,13 +4,12 @@ import subprocess
 import sys
 import re
 
-# 
 def get_wpctl_status():
     # Run wpctl status and collect its output
     wpctl_raw_output = subprocess.run(["wpctl", "status"], capture_output=True, text=True)
     # strip stdout data of wpctl status
     wpctl_stdout = wpctl_raw_output.stdout
-    # Remove ascii characters.
+    # Only leave ascii characters (remove tree structure)
     wpctl_stdout = wpctl_stdout.encode('ascii', 'ignore').decode('ascii')
     return wpctl_stdout
 
@@ -30,11 +29,9 @@ def wpctl_list(wpctl_status, *args):
         else:
             print(f"ERROR: Arg '{arg}' is unknown.", file=sys.stderr)
             sys.exit(1)
-
     items = []
     all_found = False
     media_type_found = False
-
     # Go through the lines one by one. When media type and stream device are
     # found save all the other lines until empty line is also found.
     for line in wpctl_status.splitlines():
@@ -43,8 +40,8 @@ def wpctl_list(wpctl_status, *args):
             if line.strip() == '':
                 break
             else:
-                # remove whitespace at start and end. Also remove * and spaces at
-                # the start which represents the default sink
+                # remove whitespace at start and end. Moreover, put * after the
+                # number: allow grep '*' | cut -d'*' -f1 to get default ID.
                 line = re.sub(r'^(\*?)\s*(\d+)\s*.\s*', r'\2\1\t', line.strip())
                 line = re.sub(r' \[.*$', '', line) # remove [...] at the end
                 items.append(line) #remove the dot in the ID
@@ -55,19 +52,11 @@ def wpctl_list(wpctl_status, *args):
         # If media type is found, try to find stream device: audio or video
         elif media_type_found and ("  " +stream_device+"s:" == line.lower()):
             all_found = True
-
     if not items:
         return ''
     else:
         return '\n'.join(items) + '\n'
 
-
-
-
-#print(list_sinks(wpctl_stdout))
 stream_list = wpctl_list(get_wpctl_status(), *sys.argv[1:])
 
 print(stream_list, end = '')
-
-
-
